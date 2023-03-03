ef930cf5fd654b2e42f6f417cc47e7dd3c9aa224
artem4epa <artemchepa@yandex.ru>
2023-03-03 20:01:08 +0300

    fix_price

--- backend/foodgram_api/api/services.py
+++ backend/foodgram_api/api/services.py
@@ -6,16 +6,16 @@ def create_shopping_cart(request, user):
     shopping_list = [
         f'Список покупок для: {user.first_name}\n'
     ]
-    ingredients = RecipeIngredient.objects.filter(
+    ingredient = RecipeIngredient.objects.filter(
         recipe__in_cart__user=request.user
     ).values(
         'ingredient__name',
         'ingredient__measurement_unit'
     ).annotate(amount=Sum('amount'))
-    for ingredient in ingredients:
+    for ing in ingredient:
         shopping_list.append(
-            f"{ingredient['indgredient__name']}:\
-                {ingredient['amount']}\
-                {ingredient['ingredient__measurement_unit']}"
+            f"{ing['indgredient__name']}:\
+                {ing['amount']}\
+                {ing['ingredient__measurement_unit']}"
         )
     return shopping_list
